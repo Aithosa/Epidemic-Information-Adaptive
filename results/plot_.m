8a5309c2192cb156efed6a928b19fdc6bdf97094
Aithusa <32020141+Aithosa@users.noreply.github.com>
2020-02-20 20:30:01 +0800

    total add and push

---
+++ results/plot_.m
@@ -0,0 +1,15 @@
+plot(WS_WS_w_infective(1,:)); hold on;
+% plot(WS_WS_w_infective(2,:)); hold on;
+% plot(WS_WS_w_infective(3,:)); hold on;
+plot(WS_WS_w_infective(4,:)); hold on;
+% plot(WS_WS_w_infective(5,:)); hold on;
+plot(WS_WS_w_infective(6,:)); hold on;
+plot(WS_WS_w_infective(7,:)); hold on;
+% plot(WS_WS_w_infective(8,:)); hold on;
+plot(WS_WS_w_infective(9,:)); hold on;
+% plot(WS_WS_w_infective(10,:)); hold on;
+plot(WS_WS_w_infective(11,:)); hold on;
+
+legend('a=0', 'a=0.2', 'a=0.4', 'a=0.6', 'a=0.8', 'a=1')
+% legend('a=0','a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5', 'a=0.6', 'a=0.7', 'a=0.8', 'a=0.9', 'a=1')
+grid on;
---
+++ results/plot__.m
@@ -0,0 +1,8 @@
+plot(WS_WS_beta_infective_w_02); hold on;
+plot(WS_WS_beta_infective_w_04); hold on;
+plot(WS_WS_beta_infective_w_06); hold on;
+plot(WS_WS_beta_infective_w_08); hold on;
+plot(WS_WS_beta_infective_w_1); hold on;
+
+legend('w=0.2', 'w=0.4', 'w=0.6', 'w=0.8', 'w=1')
+grid on;
