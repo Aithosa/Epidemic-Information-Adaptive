184203e56e2d087af8a3c07d5d6207a98bc58122
Aithusa <32020141+Aithosa@users.noreply.github.com>
2020-02-29 22:25:36 +0800

    add

---
+++ results/test/data/plot_BA_WS.m
@@ -0,0 +1,15 @@
+plot(BA_WS_w_infective(1,1:50)); hold on;
+% plot(BA_WS_w_infective(2,1:50)); hold on;
+% plot(BA_WS_w_infective(3,1:50)); hold on;
+plot(BA_WS_w_infective(4,1:50)); hold on;
+% plot(BA_WS_w_infective(5,1:50)); hold on;
+plot(BA_WS_w_infective(6,1:50)); hold on;
+plot(BA_WS_w_infective(7,1:50)); hold on;
+% plot(BA_WS_w_infective(8,1:50)); hold on;
+plot(BA_WS_w_infective(9,1:50)); hold on;
+% plot(BA_WS_w_infective(10,1:50)); hold on;
+plot(BA_WS_w_infective(11,1:50)); hold on;
+
+legend('w=0', 'w=0.2', 'w=0.4', 'w=0.6', 'w=0.8', 'w=1')
+% legend('a=0','a=0.1', 'a=0.2', 'a=0.3', 'a=0.4', 'a=0.5', 'a=0.6', 'a=0.7', 'a=0.8', 'a=0.9', 'a=1')
+grid on;
